d6e108d75e8c653673a408228a9f5e437737feef
User <User@WIN-8BH8LKBRQNO>
2023-11-14 14:40:50 +0500

    Add confeguration

---
+++ Kitchen.Menu/Menu.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kitchen.Menu
+{
+    public class Menu
+    {
+        public string[,] res =
+        {
+            {"Kabob", "18000"},
+            {"Somsa",    "8000"},
+            {"Norin",    "15000"},
+            {"Soup",     "12000"},
+            {"Pizza",  "75000"},
+            {"Haggi",  "25000"},
+            {"Clap",   "28000"},
+            {"Lavash", "10000"},
+            {"Juice", "1l = 8000 1.5l = 12000"},
+            {"Fanta", "1l = 10000 1.5l = 12000"},
+            {"Pepsi", "1l = 9000 1.5l = 12000"},
+            {"Cola", "1l = 11000 1.5l = 12000"},
+        };
+        public string[,] listKitchen =
+        {
+            {"Shashlik", "18000"},
+            {"Somsa",    "8000"},
+            {"Norin",    "15000"},
+            {"Soup",     "12000"},
+        };
+        public string[,] fastFoods =
+        {
+            {"Pizza",  "75000"},
+            {"Haggi",  "25000"},
+            {"Clap",   "28000"},
+            {"Lavash", "10000"},
+
+        };
+        public string[,] listDrinks =
+        {
+            {"Juice", "1l = 8000", "1.5l = 12000"},
+            {"Fanta", "1l = 10000", "1.5l = 14000"},
+            {"Pepsi", "1l = 9000", "1.5l = 13000"},
+            {"Cola", "1l = 11000", "1.5l = 15000"},
+
+        };
+        public void IsDeleteInfo(string food_name)
+        {
+            bool IsFound = false;
+            for (int i = 0; i <listKitchen.Length / 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                   if (listKitchen[i, j] == food_name)
+                   {
+                        IsFound = true;
+                        listKitchen[i, j] = null;
+                        listKitchen[i, (j + 1)] = null;
+                       
+                        
+                   }
+
+                }
+
+            }
+            for (int i = 0; i < res.Length / 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    if (res[i, j] == food_name)
+                    {
+                        IsFound = true;
+                        res[i, j] = null;
+                        res[i, (j + 1)] = null;
+
+
+                    }
+
+                }
+
+            }
+            if (IsFound == false)
+            {
+                Console.WriteLine("Not Found");
+            }
+        }
+        public void IsDeleteFastFood(string fastFood_name)
+        {
+            bool IsFound = false;
+            for (int i = 0; i < fastFoods.Length / 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    if (fastFoods[i, j] == fastFood_name)
+                    {
+                        IsFound = true;
+                        fastFoods[i, j] = null;
+                        fastFoods[i, (j + 1)] = null;
+                        
+                    }
+
+                }
+
+            }
+            if (IsFound == false)
+            {
+                Console.WriteLine("Not Found");
+            }
+        }
+        public void IsDeleteDrinks(string drink_name)
+        {
+            bool IsFound = false;
+            for (int i = 0; i < listDrinks.Length / 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (listDrinks[i, j] == drink_name)
+                    {
+                        IsFound = true;
+                        listDrinks[i, j] = null;
+                        listDrinks[i, (j + 1)] = null;
+                        listDrinks[i, (j + 2)] = null;
+                    }
+
+                }
+
+            }
+            if (IsFound == false)
+            {
+                Console.WriteLine("Not Found");
+            }
+        }
+        public void GetAllInfo()
+        {
+            for (int i = 0; i < res.Length / 2; i++)
+            {
+                int count = 0;
+                for (int j = 0; j < 2; j++)
+                {
+                    if (res[i, j] != null)
+                    {
+                        Console.Write(res[i, j] + " ");
+                    }
+                    else
+                    {
+                        count++;
+                    }
+
+                }
+                if (count != 2)
+                {
+                    Console.WriteLine();
+                }
+
+            }
+        }
+        public void ClearAllInfo()
+        {
+            if (res != null)
+            {
+                for (int i = 0; i < res.Length / 2; i++)
+                {
+                    for (int j = 0; j < 2; j++)
+                    {
+                        res[i, j] = null;
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Baza Mavjud emas");
+            }
+        }
+    }
+}
